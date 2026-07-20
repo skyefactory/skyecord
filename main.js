a28bf85521f174ea0a7809ba09eaa7fe8f5ad8bd
skye <d.ostapa@gmail.com>
2026-07-20 16:47:56 -0500

    fixed micropone issue maybe?

--- main.js
+++ main.js
@@ -6,25 +6,10 @@ const path = require('path');
 
 updateElectronApp();
 
-app.on('web-contents-created', (event, contents) => {
-    contents.session.setPermissionRequestHandler(
-        (webContents, permission, callback) => {
-            console.log("Permission requested:", permission);
-
-            if (permission === 'media') {
-                callback(true);
-                return;
-            }
-
-            callback(false);
-        }
-    );
-});
+// REMOVED: The old app.on('web-contents-created') block has been removed to avoid conflicts.
 
 const createWindow = () => {
-    // This will now execute perfectly without throwing an error
-    //Menu.setApplicationMenu(null); 
-    
+    // Note: Creating a BrowserWindow automatically initializes the defaultSession.
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -33,12 +18,15 @@ const createWindow = () => {
             contextIsolation: true
         }
     });
+
+    // Unified Permission Handler
     session.defaultSession.setPermissionRequestHandler((webContents, permission, callback) => {
-        // Array of all permissions your multimedia app requires
-        const allowedPermissions = ['audioCapture', 'videoCapture', 'display-capture'];
+        console.log("Permission requested:", permission); // This will log 'audioCapture' for microphones
+
+        const allowedPermissions = ['audioCapture', 'videoCapture', 'display-capture', 'media'];
         
         if (allowedPermissions.includes(permission)) {
-        return callback(true); // Automatically grant permission
+            return callback(true); // Automatically grant permission
         }
         
         callback(false); // Reject everything else
